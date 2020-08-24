dc7a2864e92a0346d165aeed040b45b7ad612a56
Xyconix <42036798+Xyconix@users.noreply.github.com>
2020-08-24 14:07:24 -0400

    Python 1: Variables

---
+++ main.py
@@ -0,0 +1,16 @@
+print("My creator is Xyconix")  # You can interchange single and double quotes. Double quotes used here. All strings
+# must use quotes.
+print('more strings please!! ')  # Single quotes used here.
+print(3)  # You can also print a number because it is not a string.
+a = 1  # First variable; a is referring to the value 1. a is going to represent 1. No quotes are needed because it
+# is a string, not a variable.
+print(a)  # Prints the variable a which is currently being represented as 1. This allows you to see what is contained.
+b = 2  # Another variable; b is referring to the value 2.
+print(b)  # Prints the variable b which is referring to 2 in this case.
+print(a)
+print(b)
+c = "Hello Xyconix!"
+print(c)
+b = "Yo, that's different"  # You can associate the same variable with the same character as long as it was already
+# executed before you stated the other variable. This works with strings and numbers.
+print(b)
---
+++ test.py
@@ -0,0 +1,10 @@
+# Swapping two variables
+v01 = "string 1"    # Variables v01 and v02 are given strings. Combine the variables so you do not have repeat them.
+v02 = "string 2"
+
+temp = v01  # New variable is listen in order to accomplish this.
+v01 = v02
+v02 = temp
+
+print(v01)  # Now, simply print the newly created variables above to print your string to the screen. 
+print(v02)
